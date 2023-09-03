38f7974ef027f8595327d5e4aed8acee7afb2d4a
LuigiIorio <l.iorio3@campus.unimib.it>
2023-09-03 19:49:46 +0200

    The 4 activities now implement the MVVM architecture

--- app/src/main/java/com/example/healthcoach/activities/HomeActivity.java
+++ app/src/main/java/com/example/healthcoach/activities/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.healthcoach.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,21 +13,27 @@ import com.example.healthcoach.R;
 import com.example.healthcoach.fragments.FragmentScreen1;
 import com.example.healthcoach.fragments.FragmentScreen2;
 import com.example.healthcoach.fragments.FragmentScreen3;
+import com.example.healthcoach.viewmodels.HomeViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.Nullable;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView welcomeTextView;
     private Fragment fragment1, fragment2, fragment3;
     private Fragment activeFragment;
+    private HomeViewModel homeViewModel; // Add ViewModel
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
 
         welcomeTextView = findViewById(R.id.welcomeTextView);
+
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class); // Initialize ViewModel
 
         // Initialize fragments
         fragment1 = new FragmentScreen1();
@@ -36,20 +43,16 @@ public class HomeActivity extends AppCompatActivity {
         // Set initial active fragment
         activeFragment = fragment1;
 
-        // Add the code for bottom navigation view setup here
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-        // Add the code to inflate the FragmentScreen1 fragment here
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, fragment1, "1").commit();
 
-        // Set retain instance for fragments
         fragment1.setRetainInstance(true);
         fragment2.setRetainInstance(true);
         fragment3.setRetainInstance(true);
     }
-
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
             item -> {
@@ -79,8 +82,7 @@ public class HomeActivity extends AppCompatActivity {
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
-        // Navigate back to the login page (MainActivity)
         startActivity(new Intent(HomeActivity.this, MainActivity.class));
-        finish(); // Close the current activity
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/healthcoach/viewmodels/HomeViewModel.java
@@ -0,0 +1,15 @@
+package com.example.healthcoach.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    // You can add data fields and methods related to the business logic here
+    // For example, handling data retrieval, processing, and manipulation
+
+    // Sample method to perform some business logic
+    public String getWelcomeMessage() {
+        return "Welcome to HealthCoach!";
+    }
+
+    // Add more methods as needed for your business logic
+}
